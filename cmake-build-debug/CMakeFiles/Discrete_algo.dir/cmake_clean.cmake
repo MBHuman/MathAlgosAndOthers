4a8ec8083c09c7711b90bd21e3adcebbc36f70fa
Evgeny <zenka712@mail.ru>
2021-06-05 17:43:08 +0300

    add cmakelist

---
+++ cmake-build-debug/CMakeFiles/Discrete_algo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Discrete_algo.dir/main.cpp.obj"
+  "Discrete_algo.exe"
+  "Discrete_algo.exe.manifest"
+  "Discrete_algo.lib"
+  "Discrete_algo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Discrete_algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by zenka on 05.06.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+}
