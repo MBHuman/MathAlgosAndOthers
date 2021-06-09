92d25843f850ead15a61e8772b41ad1148b18d14
Evgeny <zenka712@mail.ru>
2021-06-09 21:03:05 +0300

    add slow BWT

--- DiscreteMath/CompressionAlgos/BWT.h
+++ DiscreteMath/CompressionAlgos/BWT.h
@@ -21,29 +21,41 @@ struct Node {
 
 class BWT {
 public:
-    vector<int> reverseBWT(int N, int M, int k, string s) {
-        vector<Node> word;
-        int i = 0;
-        for(auto &u : s) {
-            Node temp;
-            temp.letter = u;
-            temp.last = i;
-            word.push_back(temp);
-            ++i;
+    string encodeBWT(string s) {
+        s += '$';
+        vector<string> strings;
+        for(int i = 0; i < s.length(); ++i) {
+            string temp;
+            for(int j = 0; j < s.length(); ++j) {
+                temp += s[(i + j) % s.length()];
+            }
+            strings.push_back(temp);
         }
-        auto comp = [](const Node a, const Node b) {
-            return a.letter < b.letter;
-        };
-        sort(word.begin(), word.end(), comp);
-        i = 0;
-        for(auto &u : word) {
-            u.current = i++;
+        sort(strings.begin(), string.end());
+        string ans;
+        for(auto u : strings) {
+            ans += u.back();
         }
-        auto comp = [](const Node a, const Node b) {
-            return a.last < b.last;
-        };
-        sort(word.begin(), word.end(), comp);
-
+        return ans;
+    }
+    string encodeDeprecatedBWT(string s) { // Slow variant
+        // We can make it fast. If we look at i and j places. Changes repeated, then we can sort only one time. not s.length()
+        // But i'm lazy to do that
+        int from = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if(s[i] == '$') {
+                from = i;
+                break;
+            }
+        }
+        vector<string> vc(s.length());
+        for(int i = 0; i < s.length(); ++i) {
+            for(int j = 0; j < s.length(); ++j) {
+                vc = s[j] + vc
+            }
+            sort(vc.begin(), vc.end());
+        }
+        return vc[from];
     }
 private:
 
